81f6a3851f284daeea7f36eeb96d6402e18fde66
anjali-t501 <134695156+anjali-t501@users.noreply.github.com>
2023-06-03 19:17:39 +0530

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -10,8 +10,9 @@ const sendToken = (user,statusCode, res) =>{
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 *1000
         ),
-        httpOnly: true,
-        sameSite: 'none', 
+       httpOnly: true,
+      sameSite: app.get("env") === "development" ? true : "none",
+      secure: app.get("env") === "development" ? false : true,
     };
 
     res.status(statusCode).cookie("token", token,options).json({
